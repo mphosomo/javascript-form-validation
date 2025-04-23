4599e0eda97a709ed8bc84a48e2a46423de17e0e
programmingwithfantom <mphosomo07@gmail.com>
2025-04-23 13:03:28 +0200

    feat: add required field to input only after form submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import "./styles.css"
+import "./styles.css";
 
-console.log("working!")
+const form = document.querySelector("form");
+
+const inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input) => {
+    input.required = true;
+  });
+});
