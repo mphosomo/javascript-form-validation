dfb5f12fc5cf75cd49d8767dc6a1335646539299
programmingwithfantom <mphosomo07@gmail.com>
2025-04-24 10:37:42 +0200

    feat: convert postal codes json data into a usable object

---
+++ src/jsonToObject.js
@@ -0,0 +1,15 @@
+import postalCodeDataJson from "./postalCodeData.json";
+
+// Turns each element of the object into an array
+const postalCodes = Object.entries(
+  postalCodeDataJson.supplemental.postalCodeData,
+);
+
+// Using the postal codes array now to create an object entry for each element
+// The object is now in the format: { postalCodeName: Regex }
+export const constraints = {};
+postalCodes.forEach((postalCode) => {
+  constraints[postalCode[0]] = postalCode[1];
+});
+
+// When performing regex checks, I can now use constraints.postalCodeName to get its Regex
