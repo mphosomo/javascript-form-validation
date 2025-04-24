36319e5993ddc9be3e99f326e09cf9d83b55bf2a
programmingwithfantom <mphosomo07@gmail.com>
2025-04-24 11:10:47 +0200

    feat: add validity check for postal codes

--- src/validityChecks.js
+++ src/validityChecks.js
@@ -1,3 +1,5 @@
+import { constraints } from "./jsonToObject";
+
 export function checkEmail() {
   const email = document.querySelector("#email");
   const emailValue = email.value;
@@ -10,7 +12,20 @@ export function checkEmail() {
   }
 }
 
-export function checkPostalCode() {}
+export function checkPostalCode() {
+  const postalCodeName = document.querySelector("#country").value;
+  const postalCodeInput = document.querySelector("#postal-code");
+
+  const constraint = new RegExp(constraints[postalCodeName], "");
+
+  // If the postal code entered by the user follows the constraint, then we don't show any error
+  if (constraint.test(postalCodeInput.value)) {
+    postalCodeInput.setCustomValidity("");
+  } else {
+    postalCodeInput.setCustomValidity(constraints[postalCodeName]);
+    postalCodeInput.reportValidity();
+  }
+}
 
 export function checkPassword() {}
 
